f0e11e47aa1f3c3b7ce4b061e2a691f36e51b3a3
RickMohr <rmohr@azavea.com>
2015-09-23 10:15:50 -0400

    wip

---
+++ opentreemap/treemap/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('treemap', '0008_instance_eco_rev'),
+        ('treemap', '0007_mapfeature_hide_at_zoom'),
+    ]
+
+    operations = [
+    ]
